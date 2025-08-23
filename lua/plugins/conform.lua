f337662c43ab5530babedf84817fcd6cbabcc0ee
juannn-j <lainfhj@proton.me>
2025-08-22 19:09:56 -0500

    update lsp, fmt, linters ..

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,9 @@ local formatters = {
   json = { "prettier" },
   markdown = { "prettier" },
   rust = { "rustfmt" },
+  go = { "gofmt" },  -- Agregado para Go
+  ruby = { "rufo" }, -- Agregado para Ruby
+  terraform = { "terraform_fmt" }, -- Agregado para Terraform
 }
 
 return {
@@ -16,7 +19,16 @@ return {
     formatters_by_ft = formatters,
   },
   init = function()
+    -- Configuración para usar Conform como formateador
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    
+    -- Automático al guardar: formatea el archivo solo si es necesario
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function()
+        vim.lsp.buf.format({ async = true })
+      end,
+    })
   end,
   keys = {
     {
@@ -25,6 +37,14 @@ return {
         require("conform").format({ lsp_format = "fallback" })
       end,
       desc = "Format with Conform",
+    },
+    -- Agregar otros atajos de teclado si lo deseas
+    {
+      "<leader>vF",
+      function()
+        require("conform").format({ lsp_format = true })
+      end,
+      desc = "Format with LSP (if available)",
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,30 +1,58 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup()
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    require("mason").setup()
 
-		-- Herramientas externas que quieres asegurar que se instalen
-		local ensure_installed = {
-			"stylua", -- Formateador Lua
-			"prettier",
-			"black",
-		}
+    -- Herramientas externas que quieres asegurar que se instalen
+    local ensure_installed = {
+      "stylua",     -- Formateador Lua
+      "prettier",   -- Formateador para JavaScript, TypeScript, etc.
+      "black",      -- Formateador Python
+      "csharpier",  -- Formateador C#
+      "eslint_d",   -- Linter para JavaScript/TypeScript (necesita Node.js)
+      "shellcheck", -- Linter para scripts shell
+      "flake8",     -- Linter para Python
+      "tflint",     -- Linter para Terraform
+      "codespell",  -- Linter para errores tipográficos
+      "ast-grep",
+    }
 
-		require("mason-tool-installer").setup({
-			ensure_installed = ensure_installed,
-		})
+    require("mason-tool-installer").setup({
+      ensure_installed = ensure_installed,
+    })
 
-		-- Lenguajes soportados por LSP
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"markdown_oxide",
-				"pylsp",
-			},
-		})
-	end,
+    -- Lenguajes soportados por LSP
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "markdown_oxide",
+        "pylsp",
+        "omnisharp",
+        -- "tsserver",
+        -- "gopls",
+        "bashls",
+        "jsonls",
+      },
+    })
+
+    -- DAPs (Debuggers)
+    -- require("mason-nvim-dap").setup({
+    --   ensure_installed = {
+    --     "python",     -- DAP para Python
+    --     "cpp",        -- DAP para C++
+    --     "csharp",     -- DAP para C#
+    --     "javascript", -- DAP para JavaScript
+    --     "java",       -- DAP para Java
+    --     "bash",       -- DAP para Bash
+    --     "go",         -- DAP para Go
+    --     "ruby",       -- DAP para Ruby
+    --     "rust",       -- DAP para Rust
+    --   },
+    -- })
+  end,
 }
